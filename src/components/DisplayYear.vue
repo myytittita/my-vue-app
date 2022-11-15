a67be0fc2b0b8b321c6546163e268814b8d7b0f8
Tittita Amonpativet <tamonpat@pratt.edu>
2022-11-15 18:57:03 -0500

    Add dropdown menu

--- src/components/DisplayYear.vue
+++ src/components/DisplayYear.vue
@@ -2,6 +2,14 @@
     <!-- create a dropdown for all the years searched -->
     <!-- create a local data property called 'currentYear' -->
     <!-- use currentYear to pick which list of facts to show -->
+    <select>
+        <option v-for="(facts, year, index) in yearfacts" v-bind:key="index">
+            <div @click="getCurrentYear">
+                {{ year }}
+            </div>
+        </option>
+    </select>
+
     <h2>🗓️ Here is what happened 🤓</h2>
     <ul v-for="(facts, year, index) in yearfacts" v-bind:key="index">
         <li v-for="(fact, index) in facts" v-bind:key="index">
@@ -13,7 +21,7 @@
 <script>
 export default {
     name: 'DisplayYear',
-    props: ['years', 'yearfacts'],
+    props: ['years', 'yearfacts', 'currentYear'],
 }
 </script>
 
@@ -38,7 +46,6 @@ li {
     margin-bottom: 1em;
     padding: 40px;
     /* position: absolute; */
-    width: 45%;
-    height: 70px;
+    height: fit-content;
 }
 </style>
